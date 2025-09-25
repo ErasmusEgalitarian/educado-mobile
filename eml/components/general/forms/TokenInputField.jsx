7db3e95b986c98d27b4a406fa96b40aae3a3f1f5
Peter <petersimondunlop1@gmail.com>
2025-09-25 14:38:31 +0200

    Text input field working but requires bug fixes

--- eml/components/general/forms/TokenInputField.jsx
+++ eml/components/general/forms/TokenInputField.jsx
@@ -1,6 +1,6 @@
     import React, { useState, useRef } from "react";
     import { View, TextInput, StyleSheet, Keyboard } from "react-native";
-import ShowAlert from "../ShowAlert";
+    import ShowAlert from "../ShowAlert";
 
     export default function TokenInputField({ length = 6, onChange }) {
     const [values, setValues] = useState(Array(length).fill("")); // ["", "", "", "", "", ""]
@@ -11,40 +11,37 @@ import ShowAlert from "../ShowAlert";
         has limited android support, 3rd party library is needed
     */
 
+    /*
     const handleChange = (value, id) => {
-        // if backspace is detected move to id - 1 (previous cell)
-        if ((value === '' || null) && id > 0) {
-            inputs.current[id - 1].focus();
-        }
-
-       // if (!/^[0-9a-zA-Z]?$/.test(value)) return; // allow only a-z 0-9
-
-       
+        if (!/^[0-9a-zA-Z]?$/.test(value)) return; // allow only a-z 0-9
         // Id is index
         const newValues = [...values];  
         newValues[id] = value;
-
-
-        if(newValues[id].length == 2){
-
-            if(id == length - 1) return;
-        
-            inputs.current[id + 1].focus();
-            newValues[id+1] = newValues[id].charAt(1);
-            newValues[id] = newValues[id].charAt(0);
-        }
-
-
         setValues(newValues);
         onChange?.(newValues.join(""));
 
-      //  if (value && id < length - 1) {
-      //      inputs.current[id + 1].focus();
-      //  }
+        if(value && id < length - 1){
+            inputs.current[id + 1].focus();
+        }
 
         // hide keyboard when last input is filled
         if (value && id === length - 1) {
             Keyboard.dismiss();
+        }
+    };
+    */
+
+    const keyPress = (key, id) => {
+        const newValues = [...values];  
+        console.log(id);
+        if(key === "Backspace" && id !== 0){
+            inputs.current[id - 1].focus();
+            newValues[id - 1] = "";
+            setValues(newValues);
+        } else {
+            newValues[id] = key;   
+            if(id !== length - 1) inputs.current[id + 1].focus();
+            setValues(newValues);
         }
     };
 
@@ -55,10 +52,9 @@ import ShowAlert from "../ShowAlert";
                     key={id}
                     style={styles.input}
                     value={val}
-                    maxLength={2}
-                    onChangeText={(text) => handleChange(text.toUpperCase(), id)}
+                    maxLength={1}
                     ref={(el) => (inputs.current[id] = el)}
-                    onKeyPress={(e) => ShowAlert("Test")}
+                    onKeyPress={({nativeEvent}) => keyPress(nativeEvent.key,id)}
                     keyboardType="visible-password"
                     textAlign="center"
                 />
