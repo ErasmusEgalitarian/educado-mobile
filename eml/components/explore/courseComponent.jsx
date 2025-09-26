983056dd6a1da360f73a86f761f0b8a47a943ef9
ceci000 <SK22VJ@student.aau.dk>
2025-09-26 11:22:58 +0200

    fixed location

---
+++ eml/components/explore/courseComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import tailwindConfig from '../../tailwind.config';
+
+const { View } = require("react-native");
+
+<Modal>
+    <View className="bg-surfaceSubtleCyan">text</View>
+</Modal>
--- eml/screens/explore/courseConfirmation.jsx
+++
@@ -1,6 +0,0 @@
-const { View } = require("react-native");
-const { default: BaseScreen } = require("../../components/general/BaseScreen");
-
-<BaseScreen>
-    <View>text</View>
-</BaseScreen>
