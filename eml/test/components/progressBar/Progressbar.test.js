446bc94f58e40a6ba415b25ee52cbd9197a32423
CelestrialDragon <gustav.tue@gmail.com>
2025-09-26 10:20:41 +0200

    update test

--- eml/test/components/progressBar/Progressbar.test.js
+++ eml/test/components/progressBar/Progressbar.test.js
@@ -1,53 +1,52 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
-import CustomProgressBar from '../../../components/exercise/Progressbar';
 import renderer from 'react-test-renderer';
+import CustomProgressBar from '../../../components/exercise/Progressbar';
 
 describe('<CustomProgressBar />', () => {
 
-  it('Displays 50%', () => {
+  it('Displays 50% (5/10)', () => {
     const component = renderer.create(
-      <CustomProgressBar progress={50} width={100} height={10} />
+      <CustomProgressBar progress={[50, 5, 10]} width={100} height={10} />
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('Displays 25%', () => {
+  it('Displays 25% (2/8)', () => {
     const component = renderer.create(
-      <CustomProgressBar progress={25} width={100} height={10} />
+      <CustomProgressBar progress={[25, 2, 8]} width={100} height={10} />
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('Displays 0% for no progress', () => {
+  it('Displays 0% for no progress (0/10)', () => {
     const component = renderer.create(
-      <CustomProgressBar progress={0} width={100} height={10} />
+      <CustomProgressBar progress={[0, 0, 10]} width={100} height={10} />
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('Displays 100% for full progress', () => {
+  it('Displays 100% for full progress (10/10)', () => {
     const component = renderer.create(
-      <CustomProgressBar progress={100} width={100} height={10} />
+      <CustomProgressBar progress={[100, 10, 10]} width={100} height={10} />
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('Limits display to 0% for negative progress', () => {
+  it('Limits display to 0% for negative progress (-2/10)', () => {
     const component = renderer.create(
-      <CustomProgressBar progress={-50} width={100} height={10} />
+      <CustomProgressBar progress={[-50, -2, 10]} width={100} height={10} />
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('Limits display to 100% for excess progress', () => {
+  it('Limits display to 100% for excess progress (15/10)', () => {
     const component = renderer.create(
-      <CustomProgressBar progress={150} width={100} height={10} />
+      <CustomProgressBar progress={[150, 15, 10]} width={100} height={10} />
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
