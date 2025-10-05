e6d0e07fb04174c524ad8a5ba9cf53866903a610
Martin Kedmenec <martin.kedmenec@outlook.com>
2025-10-05 15:27:16 +0200

    Fix #410. Add localization support

---
+++ i18n/index.ts
@@ -0,0 +1,36 @@
+import { I18n, TranslateOptions } from "i18n-js";
+import { getLocales, Locale, locale } from "expo-localization";
+import enUS from "@/i18n/locales/en-US.json";
+import ptBR from "@/i18n/locales/pt-BR.json";
+
+const isLocaleArrayValid = (locales: Locale[]) => {
+  return (
+    Array.isArray(locales) && locales.length > 0 && locales[0]?.languageTag
+  );
+};
+
+const getDeviceLocaleTag = (): string => {
+  const locales = typeof getLocales === "function" ? getLocales() : [];
+
+  if (isLocaleArrayValid(locales)) {
+    return locales[0].languageTag;
+  }
+
+  // Fallback for older SDKs
+  return (locale as string) || "en-US";
+};
+
+const i18n = new I18n({
+  "en-US": enUS,
+  "pt-BR": ptBR,
+  en: enUS,
+  pt: ptBR,
+});
+
+i18n.enableFallback = true;
+i18n.defaultLocale = "en-US";
+i18n.locale = getDeviceLocaleTag();
+
+const t = (key: string, options?: TranslateOptions) => i18n.t(key, options);
+
+export { i18n, t };
