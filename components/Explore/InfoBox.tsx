bae98cf7522bc29ddcd7d4d51524348561b5647e
Emandersen <minnyecomputer.com@gmail.com>
2025-10-10 13:36:21 +0200

    Remove unused imports from InfoBox component

--- components/Explore/InfoBox.tsx
+++ components/Explore/InfoBox.tsx
@@ -1,9 +1,6 @@
 import { View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Text from "@/components/General/Text";
-import {CardLabel} from "@/components/Explore/CardLabel";
-import CustomRating from "@/components/Explore/CustomRating";
-import CourseButton from "@/components/Explore/CourseButton";
 import * as Utility from "@/services/utils";
 import colors from "@/theme/colors";
 import type { ComponentProps } from "react";
