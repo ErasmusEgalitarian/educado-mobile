e9e4a3bc91682d17df260c689f0591000a4343e3
Emandersen <minnyecomputer.com@gmail.com>
2025-10-10 13:09:24 +0200

    Corrected to use global Course type

--- components/Explore/InfoBox.tsx
+++ components/Explore/InfoBox.tsx
@@ -7,6 +7,7 @@ import CourseButton from "@/components/Explore/CourseButton";
 import * as Utility from "@/services/utils";
 import colors from "@/theme/colors";
 import type { ComponentProps } from "react";
+import type { Course } from "@/types/course";
 
 type MaterialCommunityIconName = ComponentProps<
   typeof MaterialCommunityIcons
@@ -16,13 +17,6 @@ export interface InfoBoxItemProps {
   title: string;
   icon: MaterialCommunityIconName;
   className?: string;
-}
-
-export interface InfoBoxProps {
-  course: {
-    estimatedHours: number;
-    difficulty: number;
-  };
 }
 
 const InfoBoxItem = ({ title, icon, className }: InfoBoxItemProps) => {
@@ -36,7 +30,7 @@ const InfoBoxItem = ({ title, icon, className }: InfoBoxItemProps) => {
   );
 };
 
-const InfoBox = ({ course }: InfoBoxProps) => {
+const InfoBox = ({ course }: { course: Course }) => {
   return (
     <View className="w-full flex-col rounded-2xl border border-solid border-grayScale px-4 py-1">
       <InfoBoxItem
