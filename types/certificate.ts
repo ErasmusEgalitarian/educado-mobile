7fa07b16d54596dd048752ba6790724aedae7644
AugustGrane <augustgrane@gmail.com>
2025-10-16 14:13:23 +0200

    Certificate type

---
+++ types/certificate.ts
@@ -0,0 +1,11 @@
+export interface Certificate {
+  studentFirstName: string;
+  studentLastName: string;
+  courseCategory: string;
+  courseId: string;
+  studentId: string;
+  estimatedCourseDuration: number;
+  courseName: string;
+  dateOfCompletion: string;
+  courseCreator: string;
+}
